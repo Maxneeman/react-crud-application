c8f128fcb5f80481b59fd85979aaee2765c3055f
Maxneeman <nmaxneeman54@gmail.com>
2022-01-08 18:56:59 +0300

    set values according to keys from Local Storage

--- src/components/update.js
+++ src/components/update.js
@@ -20,14 +20,14 @@ export default function Update() {
             <Form className="create-form">
                 <Form.Field>
                     <label>First Name</label>
-                    <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
+                    <input placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
                     <label>Last Name</label>
-                    <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
+                    <input placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
+                    <Checkbox label='I agree to the Terms and Conditions' checked={checkbox} onChange={(e) => setCheckbox(!checkbox)}/>
                 </Form.Field>
                 <Button type='submit'>Update</Button>
             </Form>
