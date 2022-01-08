c57488570df90a967ff8bd66cb03d00b8483e129
Maxneeman <nmaxneeman54@gmail.com>
2022-01-08 18:35:16 +0300

    renamed the fields

--- src/components/read.js
+++ src/components/read.js
@@ -6,19 +6,17 @@ export default function Read() {
             <Table singleLine>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Registration Date</Table.HeaderCell>
-                        <Table.HeaderCell>E-mail address</Table.HeaderCell>
-                        <Table.HeaderCell>Premium Plan</Table.HeaderCell>
+                        <Table.HeaderCell>First Name</Table.HeaderCell>
+                        <Table.HeaderCell>Last Name</Table.HeaderCell>
+                        <Table.HeaderCell>Checked</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
                     <Table.Row>
-                        <Table.Cell>John Lilki</Table.Cell>
-                        <Table.Cell>September 14, 2013</Table.Cell>
-                        <Table.Cell>jhlilk22@yahoo.com</Table.Cell>
-                        <Table.Cell>No</Table.Cell>
+                        <Table.Cell>Maxneeman</Table.Cell>
+                        <Table.Cell>Ng'unda</Table.Cell>
+                        <Table.Cell>Yes</Table.Cell>
                     </Table.Row>
                 </Table.Body>
             </Table>
