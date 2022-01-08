7e29dcbe20979d90e63b1bcd5ac0df803cede3b7
Maxneeman <nmaxneeman54@gmail.com>
2022-01-08 18:37:07 +0300

    send the GET Request to get the data from the API

--- src/components/read.js
+++ src/components/read.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import { Table } from 'semantic-ui-react'
 export default function Read() {
+    useEffect(() => {
+       
+    }, [])
     return (
         <div>
             <Table singleLine>
