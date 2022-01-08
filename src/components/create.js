d629b9dac66c2ef7b3b39ddbc9a4e1d816cea029
Maxneeman <nmaxneeman54@gmail.com>
2022-01-08 18:21:57 +0300

    capture states and send them to the console

--- src/components/create.js
+++ src/components/create.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import './App.css'
 import React, { useState } from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
@@ -7,21 +6,26 @@ export default function Create() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [checkbox, setCheckbox] = useState(false);
+    const postData = () => {
+        console.log(firstName);
+        console.log(lastName);
+        console.log(checkbox);
+}
     return (
         <div>
             <Form className="create-form">
                 <Form.Field>
                     <label>First Name</label>
-                    <input placeholder='First Name' />
+                    <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
                     <label>Last Name</label>
-                    <input placeholder='Last Name' />
+                    <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <Checkbox label='I agree to the Terms and Conditions' />
+                <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
                 </Form.Field>
-                <Button type='submit'>Submit</Button>
+                <Button onClick={postData} type='submit'>Submit</Button>
             </Form>
         </div>
     )
