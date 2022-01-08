380f4f5d87477f4ed2501bed349b4dc7199ee3d0
Maxneeman <nmaxneeman54@gmail.com>
2022-01-08 18:53:59 +0300

    useEffect to get data stored in Local Storage

--- src/components/update.js
+++ src/components/update.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 import axios from 'axios';
 
@@ -6,6 +6,14 @@ export default function Update() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [checkbox, setCheckbox] = useState(false);
+    const [id, setID] = useState(null);
+
+    useEffect(() => {
+        setID(localStorage.getItem('ID'))
+        setFirstName(localStorage.getItem('First Name'));
+        setLastName(localStorage.getItem('Last Name'));
+        setCheckbox(localStorage.getItem('Checkbox Value'))
+}, []);
 
     return (
         <div>
