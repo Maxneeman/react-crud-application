f49d304371680b5b8bfc07f64de3f5ca5ecf4f38
Maxneeman <nmaxneeman54@gmail.com>
2022-01-08 18:04:44 +0300

    add heading with its class in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="main">
 
-          Learn React
+      <h1 className="main-header">React Crud Operations</h1>  
  
     </div>
   );
